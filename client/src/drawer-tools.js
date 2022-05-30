23d8b4a4a38850497d7a295d86f55eedefd59b0b
Peter Anderson <pete.anderson@gmail.com>
2022-05-30 15:50:31 +0200

    --add: next image if it exists

--- client/src/drawer-tools.js
+++ client/src/drawer-tools.js
@@ -4,47 +4,80 @@ import QuestionIcon from "./icons/question";
 
 export default function DrawerTool({ wordToDrawer, handleColorChange, color }) {
     const [src, setSrc] = useState(null);
+    const [nextUrl, setNextUrl] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         setSrc("");
     }, [wordToDrawer]);
 
     async function handleClick() {
-        console.log("clicked");
-        const response = await fetch(
-            `https://api.pexels.com/v1/search?query=${wordToDrawer}&per_page=1`,
-            {
-                headers: {
-                    Authorization:
-                        "563492ad6f9170000100000112a0a887569748d49276462a0a9d2c59",
-                },
-                query: wordToDrawer,
-                per_page: 5,
-                size: "medium",
-            }
+        const data = await fetchImage(
+            `https://api.pexels.com/v1/search?query=${wordToDrawer}&per_page=1`
         );
-        const data = await response.json();
-        console.log("data: ", data);
         if (data.photos.length === 0) {
             return setSrc("");
         }
         setSrc(data.photos[0].src.medium);
+        setNextUrl(data.next_page);
     }
+
+    async function fetchImage(url) {
+        setIsLoading(true);
+        const response = await fetch(url, {
+            headers: {
+                Authorization:
+                    "563492ad6f9170000100000112a0a887569748d49276462a0a9d2c59",
+            },
+            query: wordToDrawer,
+            per_page: 5,
+            size: "medium",
+        });
+        const data = await response.json();
+        setIsLoading(false);
+        return data;
+    }
+
+    async function handleImageClick() {
+        if (!nextUrl) {
+            return;
+        }
+        const data = await fetchImage(nextUrl);
+        if (data.photos.length === 0) {
+            return setSrc("");
+        }
+        setSrc(data.photos[0].src.medium);
+        setNextUrl(data.next_page);
+    }
+
     return (
         <div className="extra-wrapper extra">
             <Picker color={color} handleColorChange={handleColorChange} />
 
             <div className="find-image-wrapper">
-                <div onClick={handleClick} className="centered what-the-heck">
-                    What the heck does a {wordToDrawer} look like?
+                <div onClick={handleClick} className="what-the-heck">
+                    What the heck does a <span>{wordToDrawer}</span> look like?
                 </div>
                 <div className="image-wrapper centered">
                     {!src && (
                         <div className="question-icon" onClick={handleClick}>
-                            <QuestionIcon />
+                            {isLoading ? (
+                                <div>loading...</div>
+                            ) : (
+                                <QuestionIcon />
+                            )}
                         </div>
                     )}
-                    {src && <img className="image-to-draw" src={src} />}
+                    {src &&
+                        (isLoading ? (
+                            <div>loading...</div>
+                        ) : (
+                            <img
+                                className="image-to-draw"
+                                src={src}
+                                onClick={handleImageClick}
+                            />
+                        ))}
                 </div>
             </div>
         </div>
